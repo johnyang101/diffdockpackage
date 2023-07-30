6d7c21cb936a19181800d5ee5e56ec5772ae9578
John Yang <johnyang@mit.edu>
2023-07-29 21:44:42 -0700

    change to diffdock package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="diffdock_pip",
+    name="diffdockpackage",
     version="0.1",
     author="Giacomo Corso",
     author_email="author@domain.com", 
